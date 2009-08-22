b32a063821f8d92dc8a24c04acf95273107b3b1b
denniscm20 <denniscm20@da8d6946-189a-11de-a6cc-771c510851eb>
2009-08-22 23:28:58 +0000

    Added Default values to parameters

--- v2/puzzle/index.php
+++ v2/puzzle/index.php
@@ -3,7 +3,7 @@
 	require_once ("./config.php");
 
 	$page = DEFAULT_LOGOUT_PAGE;
-	$piece = "";
+	$piece = DEFAULT_PIECE;
 	$event = DEFAULT_EVENT;
 	date_default_timezone_set(TIMEZONE);
 	
@@ -12,7 +12,7 @@
 	if (isset($_SESSION["User"])) {
         session_regenerate_id();
 	    $page = (isset($_GET["Page"]))?$_GET["Page"]:DEFAULT_LOGIN_PAGE;
-	    $piece = (isset($_GET["Piece"]))?$_GET["Piece"]:"";
+	    $piece = (isset($_GET["Piece"]))?$_GET["Piece"]:DEFAULT_PIECE;
 	    $event = (isset($_GET["Event"]))?$_GET["Event"]:"";
 	}
 	
