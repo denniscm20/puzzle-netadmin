dfc066fa17e3164781e5c18ae56d751778786369
denniscm20 <denniscm20@da8d6946-189a-11de-a6cc-771c510851eb>
2009-09-14 00:52:15 +0000

    Clase Select modificada.

--- v2/puzzle/Lib/Html/Select.php
+++ v2/puzzle/Lib/Html/Select.php
@@ -1,61 +1,105 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/*
+ * Lib/Html/Select.php - Copyright 2009 Dennis Cohn Muroy
+ *
+ * This file is part of puzzle.
+ *
+ * puzzle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * puzzle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with puzzle.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+require_once PATH_LIB.'Html/Html.php';
 
+/**
+ * @package /Lib/
+ * @subpackage /Html/
+ * @class Input
+ * @author Dennis Stephen Cohn Muroy
+ * @version 1.0
+ */
+class Lib_Html_Select extends Lib_Html_Html {
 
-    /**
-     * Retorna un componente html del tipo combobox
-     *
-     * @param string $pName Nombre del componente.
-     * @param array $pValue Valores que almacenara el componente.
-     * @param mixed $pValueName Nombre del campo o indice que contiene el value
-     * @param string $pText Nombre del campo o indice a mostrar
-     * @param string $pStyle Estilo css que utilizará el componente.
-     * @param string $pEvent Evento a ejecutar al seleccionar una opcion del combo box
-     * @return string cadena html para formar el combobox.
-     */
-    public static function ComboBox($pName, $pValue, $pValueName = "0", $pText = "", $pTabIndex = 0, $pSelectedID = "0", $pStyle = "", $pEvent = "") {
-        $lComboBox = "<select";
-        $lComboBox .= " name = \"".$pName."\"";
-        $lComboBox .= " tabindex = \"".$pTabIndex."\"";
-        $lComboBox .= " class = \"".$pStyle."\"";
-        $lComboBox .= " onchange = \"".$pEvent."\"";
-        $lComboBox .= ">\n";
-        $lComboBox .= "<option value=\"0\">Seleccione</option>\n";
-        if (is_array($pValue)) {
-            for ($i = 0; $i < count($pValue); $i++) {
-                $objeto = $pValue[$i];
-                $lComboBox .= "<option value=\"".$objeto->$pValueName."\" ".($objeto->$pValueName == $pSelectedID?"selected":"").">".$objeto->$pText."</option>\n";
-            }
-        }
-        $lComboBox .= "</select>";
+    private $multiple = false;
+    private $size = 0;
+    private $attributeName = "";
+    private $attributeId = "";
 
-        return $lComboBox;
+    public function __construct($id, $value, $label, $attributeName = "", $attributeId = "", $tabindex = 0, $accessKey = '')
+    {
+        parent::__construct($id, $value, $label, $tabindex, $accessKey);
+        $this->multiple = false;
+        $this->attributeName = $attributeName;
+        $this->attributeId = $attributeId;
     }
 
-    /**
-     * Retorna un componente html del tipo lista
-     *
-     * @param string $pName Nombre del componente.
-     * @param array $pValue Valores que almacenara el componente.
-     * @param integer $pSize Items visibles en la lista.
-     * @param mixed $pValueName Nombre del campo o indice que contiene el value
-     * @param string $pText Nombre del campo o indice a mostrar
-     * @param string $pStyle Estilo css que utilizará el componente.
-     * @return string cadena html para formar la lista.
-     */
-    public static function ListItems($pName, $pValue, $pSize, $pValueName, $pText, $pTabIndex = 0, $pStyle = "") {
-        $lList = "<select";
-        $lList .= " name = \"".$pName."\" multiple=\"yes\"";
-        $lList .= " tabindex = \"".$pTabIndex."\"";
-        $lList .= " class = \"".$pStyle."\"";
-        if (isset($pSize)) {
-            $lList .= " size = \"".$pSize."\"";
+    protected function show()
+    {
+        $basic = parent::show();
+        $extra = $this->showExtra();
+        $events = $this->showEvents();
+        $label = "<span class=\"label\">".$this->showLabel()."</span>";
+        $own = $this->multiple===true?"multiple=\"yes\" size=\"".$this->size."\"":"";
+        $select = sprintf($basic, $own, $extra, $events);
+        $select = "<span class=\"field\"><select ".$select." >\n%s</select>\n</span>";
+        return $label.$select;
+    }
+
+    protected function showOptions($selectedID = 0)
+    {
+        $options = $this->multiple === false?"<option value=\"0\">Seleccione</option>\n":"";
+        if (is_array($this->value)) {
+            $count = count($this->value);
+            for ($i = 0; $i < $count; $i++) {
+                $object = $pValue[$i];
+                $options .= "<option value=\"";
+                if (is_object($object)) {
+                    $options .= $object->{$this->attributeId}."\" ";
+                    $options .= ($object->{$this->attributeId} == $selectedID?"selected = \"selected\"":"").">";
+                    $options .= $object->{$this->attributeName};
+                } else {
+                    $options .= $object."\" ";
+                    $options .= ($i == $selectedID?"selected = \"selected\"":"").">";
+                    $options .= $object;
+                }
+            }
         }
-        $lList .= ">";
+        $options .= "</option>\n";
+    }
+
+    public function showComboBox($pName, $pValue, $pValueName = "0", $pText = "", $selectedID = "0", $tooltip= "", $class = "", $style = "")
+    {
+        $this->type = false;
+        $this->tooltip = $tooltip;
+        $this->style = $style;
+        $this->class = $class;
+        $element = $this->show();
+        $element = sprintf($element,$this->showOptions($selectedID));
+        return $element;
+    }
+
+    public function showItemList($pName, $pValue, $size, $pValueName, $pText, $pTabIndex = 0, $pStyle = "")
+    {
+        $this->type = true;
+        $this->tooltip = $tooltip;
+        $this->style = $style;
+        $this->class = $class;
+        $this->size = $size;
+        $element = $this->show();
+        $element = sprintf($element,$this->showOptions($selectedID));
+        return $element;
+
+        
         if (is_array($pValue)) {
             for ($i = 0; $i < count($pValue); $i++) {
                 $objeto = $pValue[$i];
@@ -66,5 +110,7 @@
 
         return $lList;
     }
+
+}
 
 ?>
